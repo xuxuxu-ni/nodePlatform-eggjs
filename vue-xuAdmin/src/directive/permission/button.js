ca7ebb6f7087e4039630164ee5ddae1e1207fb46
nirongxu <595485548@qq.com>
2020-09-29 09:58:44 +0800

    修复按钮再table的插槽中,没有权限,按钮也不会隐藏,感谢@hanpanapn的反馈

--- vue-xuAdmin/src/directive/permission/button.js
+++ vue-xuAdmin/src/directive/permission/button.js
@@ -15,8 +15,16 @@ export default {
         } else {
           el.parentNode.removeChild(el)
         }
+      },
+      inserted: function (el, binding) {
+        let btnId = binding.value
+        let userInfo = JSON.parse(localStorage.getItem("info"))
+        if ((btnId && userInfo.permissionButton.indexOf(btnId) !== -1) || userInfo.role === "超级管理员") {
+          return false
+        } else {
+          el.parentNode.removeChild(el)
+        }
       }
     })
   }
 }
-
